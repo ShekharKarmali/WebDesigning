4d4f9075a20c8ca7cee967f0f77ff332448eefaa
Shekhar Karmali <karmalishekhar1055@gmail.com>
2020-01-27 11:44:22 +0530

    first phase of website for deploy

--- e_commweb/shop/models.py
+++ e_commweb/shop/models.py
@@ -23,3 +23,30 @@ class contact(models.Model):
 
     def __str__(self):
         return self.name
+
+class Orders(models.Model):
+    order_id=models.AutoField(primary_key=True)
+    items_json=models.CharField(max_length=1000)
+    name =models.CharField(max_length=100)
+    email =models.CharField(max_length=50)
+    address =models.CharField(max_length=200)
+    city =models.CharField(max_length=50)
+    state =models.CharField(max_length=50)
+    phone =models.IntegerField(default=0)
+    zip_code =models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+class OrderUpdate(models.Model):
+    update_id=models.AutoField(primary_key=True)
+    order_id=models.IntegerField(default=0)
+    update_desc=models.CharField(max_length=500)
+    timestamp=models.DateField(auto_now_add=True)
+     
+    def __str__(self):
+        return self.update_desc[0:7] + "....."
+
+
+
--- e_commweb/shop/views.py
+++ e_commweb/shop/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from . models import Product
 from . models import contact
+from . models import Orders
+from . models import OrderUpdate
 from math import ceil
+import json
 
 # Create your views here.
 def index(request):
@@ -29,8 +32,24 @@ def about(request):
     return render(request,'shop/About.html')
 
 def tracker(request):
-    return render(request,'shop/tracker.html') 
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps(updates, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{}')
+        except Exception as e:
+            return HttpResponse('{}')
 
+    return render(request, 'shop/tracker.html')
 def Contact(request):
     if request.method=="POST":
         name=request.POST.get('name','')
@@ -52,5 +71,45 @@ def productview(request,myid):
     product = Product.objects.filter(id=myid)
     return render(request, 'shop/prodView.html', {'product':product[0]})
 
+# def checkout(request):
+#     if request.method=="POST":
+#         items_json=request.POST.get('itemsJson','')
+#         name=request.POST.get('name','')
+#         email=request.POST.get('email','')
+#         phone=request.POST.get('phone','')
+#         address=request.POST.get('address','') + " " + request.POST.get('landmark','') 
+#         city=request.POST.get('city','')
+#         state=request.POST.get('state','')
+#         zip_code=request.POST.get('zip_code','')
+#         order= Orders(items_Json=items_Json,name=name,email=email,phone=phone,address=address,
+#                        city=city,state=state,zip_code=zip_code)
+#         order.save()
+#         # thank=True
+#         # id=order.order_id
+#        #return render(request,'shop/checkout.html',{'thank':thank, 'id':id})
+#     return render(request,'shop/checkout.html')
+
 def checkout(request):
-    return render(request,'shop/checkout.html')
+    if request.method=="POST":
+        items_json = request.POST.get('itemsJson', '')
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        address = request.POST.get('address', '') + " " + request.POST.get('landmark', '')
+        city = request.POST.get('city', '')
+        state = request.POST.get('state', '')
+        zip_code = request.POST.get('zip_code', '')
+        phone = request.POST.get('phone', '')
+        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+                       state=state, zip_code=zip_code, phone=phone)
+        order.save()
+        update=OrderUpdate(order_id=order.order_id,update_desc="The Order has been placed")
+        update.save()
+        thank = True
+        id = order.order_id
+        return render(request, 'shop/checkout.html', {'thank':thank, 'id': id})
+    return render(request, 'shop/checkout.html')
+
+
+
+
+    
